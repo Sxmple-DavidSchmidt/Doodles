5a5dd3f614c2727f9c58e967245470ee09a274d6
CuttingEdge <62205605+Sxmple-DavidSchmidt@users.noreply.github.com>
2023-04-28 11:27:16 +0200

    Update homework.cs

--- homework.cs
+++ homework.cs
@@ -4,11 +4,11 @@ class HelloWorld {
     static void Main() {
         float fahrenheit, celsius;
         celsius = 0;
-        fahrenheit = 1.8f * celsius + 32;
+        fahrenheit = 9.0f/5.0f * celsius + 32;
         Console.WriteLine("{0} Celsius entspricht {1} fahrenheit", celsius, fahrenheit);
         
         celsius = (float) Convert.ToDouble("20");
-        fahrenheit = 1.8f * celsius + 32;
+        fahrenheit = 9.0f/5.0f * celsius + 32;
         Console.WriteLine("{0} Celsius entspricht {1} fahrenheit", celsius, fahrenheit);
     }
 }
